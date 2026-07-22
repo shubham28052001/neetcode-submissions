bd1a779c29e9ad61a92282f0b22a0070c2775135
Shubham Sanodiya <shubhamsanodiya2805@gmail.com>
2026-07-22 14:09:14 +0530

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.js
@@ -0,0 +1,22 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        let left = 0
+        let maxlength = 0
+        let set = new Set();
+
+        for (let right = 0; right < s.length; right++) {
+            while (set.has(s[right])) {
+                set.delete(s[left])
+                left++
+            }
+            set.add(s[right])
+            maxlength = Math.max(maxlength, right - left + 1);
+        }
+
+        return maxlength;
+    }
+}
